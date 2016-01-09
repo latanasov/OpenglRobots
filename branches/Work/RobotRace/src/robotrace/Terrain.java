a2b5bde758debfd4efcbb392557f3bea49a33527
QiongWu <QiongWu@39c68fb2-be14-4621-9843-43df347e53e8>
2016-01-09 21:17:55 +0000

    update tangent

--- branches/Work/RobotRace/src/robotrace/Terrain.java
+++ branches/Work/RobotRace/src/robotrace/Terrain.java
@@ -1,8 +1,20 @@
 package robotrace;
 
 import com.jogamp.opengl.util.gl2.GLUT;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import static javax.media.opengl.GL.GL_LINEAR;
+import static javax.media.opengl.GL.GL_LINES;
+import static javax.media.opengl.GL.GL_RGBA;
+import static javax.media.opengl.GL.GL_RGBA8;
+import static javax.media.opengl.GL.GL_TEXTURE_2D;
+import static javax.media.opengl.GL.GL_TEXTURE_MAG_FILTER;
+import static javax.media.opengl.GL.GL_TEXTURE_MIN_FILTER;
 import static javax.media.opengl.GL.GL_TRIANGLES;
+import static javax.media.opengl.GL.GL_UNSIGNED_BYTE;
 import javax.media.opengl.GL2;
+import static javax.media.opengl.GL2GL3.GL_QUADS;
+import static javax.media.opengl.GL2GL3.GL_TEXTURE_1D;
 import javax.media.opengl.glu.GLU;
 
 /**
@@ -14,8 +26,9 @@ class Terrain {
     /**
      * Can be used to set up a display list.
      */
-    public Terrain() {
+    public Terrain() { 
         // code goes here ...
+        
         
     }
 
@@ -23,10 +36,22 @@ class Terrain {
      * Draws the terrain.
      */
     public void draw(GL2 gl, GLU glu, GLUT glut) {
-        // code goes here ...
+        // code goes here ..
+        byte textLine[]=new byte[16];
+        textLine = calculateTextureColor(0);
+        Buffer buf=ByteBuffer.wrap(textLine);
         
-        gl.glColor3f(0f, 1f, 0f); 
-        gl.glBegin(GL_TRIANGLES);
+        gl.glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        gl.glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        
+        //gl.glTexImage1D(GL_TEXTURE_1D, 0, GL_RGBA, 4, 0, GL_RGBA, GL_UNSIGNED_BYTE, buf);
+     
+        
+        gl.glDisable(GL_TEXTURE_2D);
+        gl.glEnable(GL_TEXTURE_1D);
+        gl.glTexImage1D(GL_TEXTURE_1D, 0, GL_RGBA, 4, 0, GL_RGBA, GL_UNSIGNED_BYTE, buf);
+        
+        //gl.glBegin(GL_TRIANGLES);
             float x1 = 0;
             float x2 = 0;
             float y1 = 0;
@@ -37,40 +62,84 @@ class Terrain {
             
             for (int i = -50; i< 50; i++ )
             {
- 
+                for (int j = -50; j< 50; j++ )
+                {
+                   //y1 = i;
+                   //y2 = i+1;
+                   if (heightAt(20*i/NV, 20*j/NV) <0){
+                   gl.glBegin(GL_TRIANGLES);
+                   gl.glTexCoord1f(0.1f);
+                   gl.glVertex3f(20*i/NV, 20*j/NV, heightAt(20*i/NV, 20*j/NV));
+                   gl.glTexCoord1f(0.1f);
+                   gl.glVertex3f(20*i/NV, 20*(j+1)/NV, heightAt(20*i/NV, 20*(j+1)/NV));
+                   gl.glTexCoord1f(0.1f);
+                   gl.glVertex3f(20*(i+1)/NV, 20*(j+1)/NV, heightAt(20*(i+1)/NV, 20*(j+1)/NV));
                 
-                
-                /*for (int j = -50; j< 50; j++ ){
-                y1 = i;
-                y2 = i+1;
-                gl.glVertex3f(2/5*i, j, heightAt(i, j));
-                gl.glVertex3f(i, j+1, heightAt(i, j+1));
-                gl.glVertex3f(i+1, j+1, heightAt(i+1, j+1));
-                
-                gl.glVertex3f(i, j, heightAt(i, j));
-                gl.glVertex3f(i+1, j, heightAt(i+1, j));
-                gl.glVertex3f(i+1, j+1, heightAt(i+1, j+1));*/
-                for (int j = -50; j< 50; j++ ){
-                y1 = i;
-                y2 = i+1;
-                gl.glVertex3f(20*i/NV, 20*j/NV, heightAt(20*i/NV, 20*j/NV));
-                gl.glVertex3f(20*i/NV, 20*(j+1)/NV, heightAt(20*i/NV, 20*(j+1)/NV));
-                gl.glVertex3f(20*(i+1)/NV, 20*(j+1)/NV, heightAt(20*(i+1)/NV, 20*(j+1)/NV));
-                
-                gl.glVertex3f(20*i/NV, 20*j/NV, heightAt(20*i/NV, 20*j/NV));
-                gl.glVertex3f(20*(i+1)/NV, 20*j/NV, heightAt(20*(i+1)/NV, 20*j/NV));
-                gl.glVertex3f(20*(i+1)/NV, 20*(j+1)/NV, heightAt(20*(i+1)/NV, 20*(j+1)/NV));
+                   gl.glTexCoord1f(0.1f);
+                   gl.glVertex3f(20*i/NV, 20*j/NV, heightAt(20*i/NV, 20*j/NV));
+                   gl.glTexCoord1f(0.1f);
+                   gl.glVertex3f(20*(i+1)/NV, 20*j/NV, heightAt(20*(i+1)/NV, 20*j/NV));
+                   gl.glTexCoord1f(0.1f);
+                   gl.glVertex3f(20*(i+1)/NV, 20*(j+1)/NV, heightAt(20*(i+1)/NV, 20*(j+1)/NV));
+                   gl.glEnd();}
                 }
 
-                }
-            
-
-                   
-            
-                  
-                
-        gl.glEnd();
+            }        
+        //gl.glEnd();
+        
+        
+        
+        /*gl.glBegin(GL_QUADS);
+           gl.glTexCoord1f(0f);
+           gl.glVertex3f(0.0f,0.0f,0.0f);
+           gl.glTexCoord1f(0.1f);
+           gl.glVertex3f(5.0f,0.0f,0.0f);
+           gl.glTexCoord1f(0.1f);
+           gl.glVertex3f(5.0f,5.0f,0.0f);
+           gl.glTexCoord1f(0f);
+           gl.glVertex3f(0f,5.0f,0.0f);
+        gl.glEnd();*/
+        
+        gl.glDisable(GL_TEXTURE_1D);
+        
+        
+        
+        
     }
+    
+    private byte[] calculateTextureColor(int color)
+    {
+        int k =0;
+        //int water[] = {0,0,255,255};
+        //int sand[] = {255,128,0,255};
+        //int grass[] = {0,153,0,255};
+        
+        int texture[]={0,0,0,0};
+        
+        if (color == 0)
+        {
+           texture= new int[] {0,0,255,255}; // color for water
+        }
+        else if (color == 1)
+        {
+           texture= new int[] {255,128,0,255}; // color for sand
+        }
+        else if (color == 2)
+        {
+           texture= new int[] {0,153,0,255}; // color for grass
+        } 
+        
+        byte textLine[]=new byte[16];
+        
+        for (k=0; k<=2; k+=2)
+        {
+            textLine[4*k] = (byte) texture[0];
+            textLine[4*k+1] = (byte) texture[1];
+            textLine[4*k+2] = (byte) texture[2];
+            textLine[4*k+3] = (byte) texture[3];   
+        }
+        return textLine;
+    } 
 
     /**
      * Computes the elevation of the terrain at (x, y).
